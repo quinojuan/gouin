70fce92d7bb301f720781f8f238972a8bd0b028e
Juan Quino <quinojuan@gmail.com>
2023-01-03 12:19:46 -0300

    create customer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar";
+import CreateCustomer from "./components/CreateCustomer";
 
 function App() {
   return (
     <div className="App">
       <ButtonAppBar />
+      <CreateCustomer/>
     </div>
   );
 }
